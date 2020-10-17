65080b919af98117cebf3527a00968d1eeb24bdf
Irina Diachenco <bilairina@ukr.net>
2020-10-17 12:40:34 +0300

    все картинки

--- js/gallery-code.js
+++ js/gallery-code.js
@@ -1,2 +1,27 @@
 import images from './gallery-items.js';
-console.log(Images);
+console.log(images)
+const galleryContainer = document.querySelector('.js-gallery');
+const galleryItemMarkup = createGalleryItemsMarkup(images);
+function createGalleryItemsMarkup(images) {
+    return images.map(image => {
+       return `
+<li class="gallery__item">
+        <a
+            class="gallery__link"
+            href="${image.original}"
+        >
+            <img
+                class="gallery__image"
+                src="${image.preview}"
+                data-source="${image.original}"
+                alt="${image.description}"
+            />
+        </a>
+    </li>`;
+    
+    }).join('');
+};
+    
+
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemMarkup)
